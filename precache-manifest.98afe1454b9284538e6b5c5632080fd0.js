b13e8024c0acfdb76e2a205b844e0a9046d44b79
Naveen <nvnpanwar123@gmail.com>
2023-06-22 00:46:17 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.98afe1454b9284538e6b5c5632080fd0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3e01035161f5fbd471a09bdc940440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad2fc60b26c263240359",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "3cca5d4e124c712cafb2",
+    "url": "/static/css/main.bc3c1102.chunk.css"
+  },
+  {
+    "revision": "ad2fc60b26c263240359",
+    "url": "/static/js/2.aa43b83d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.aa43b83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cca5d4e124c712cafb2",
+    "url": "/static/js/main.3938367f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "86c0af27a95d55b3ea8a47e411bda3ec",
+    "url": "/static/media/myProfile.86c0af27.jpg"
+  },
+  {
+    "revision": "6f674f6835c67f59b3aa3a8d99af10f0",
+    "url": "/static/media/p1.6f674f68.jpg"
+  },
+  {
+    "revision": "930daa18072f9055328d976ccfbbf706",
+    "url": "/static/media/p1.930daa18.jpg"
+  },
+  {
+    "revision": "aa769ff3a68406fab4325027694598af",
+    "url": "/static/media/p1.aa769ff3.jpg"
+  },
+  {
+    "revision": "0a9103c82e0d2c727e388f6774d11275",
+    "url": "/static/media/p2.0a9103c8.jpg"
+  },
+  {
+    "revision": "be5f4e100c1d07731f928496cf335568",
+    "url": "/static/media/p2.be5f4e10.jpg"
+  },
+  {
+    "revision": "dc98c68de43ea913af07526a11ad208c",
+    "url": "/static/media/p2.dc98c68d.jpg"
+  }
+]);
